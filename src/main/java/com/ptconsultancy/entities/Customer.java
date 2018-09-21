23e6681bdd506dcffc8f52e2aec5289eb9aaa858
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-09-21 08:34:41 +0100

    master
    Addition of basic customer entity

---
+++ src/main/java/com/ptconsultancy/entities/Customer.java
@@ -0,0 +1,50 @@
+package com.ptconsultancy.entities;
+
+import com.ptconsultancy.domain.datamodels.Address;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String forename;
+    private String surname;
+    private Address primaryAddress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Address getPrimaryAddress() {
+        return primaryAddress;
+    }
+
+    public void setPrimaryAddress(Address primaryAddress) {
+        this.primaryAddress = primaryAddress;
+    }
+}
